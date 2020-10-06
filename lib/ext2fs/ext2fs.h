4ef85c12f9e0b8665cea5e77806caf59cd90888a
Harshad Shirwadkar <harshadshirwadkar@gmail.com>
2020-10-06 16:41:16 -0700

    ext2fs: add new APIs needed for fast commits
    This patch adds the following new APIs:
    
    Count the total number of blocks occupied by inode including
    intermediate extent tree nodes.
    extern blk64_t ext2fs_count_blocks(ext2_filsys fs, ext2_ino_t ino,
                                           struct ext2_inode *inode);
    
    Convert ext3_extent to ext2fs_extent.
    extern void ext2fs_convert_extent(struct ext2fs_extent *to,
                                           struct ext3_extent *from);
    
    Signed-off-by: Harshad Shirwadkar <harshadshirwadkar@gmail.com>

--- lib/ext2fs/ext2fs.h
+++ lib/ext2fs/ext2fs.h
@@ -1332,6 +1332,10 @@ extern errcode_t ext2fs_extent_fix_parents(ext2_extent_handle_t handle);
 extern size_t ext2fs_max_extent_depth(ext2_extent_handle_t handle);
 extern errcode_t ext2fs_fix_extents_checksums(ext2_filsys fs, ext2_ino_t ino,
 					      struct ext2_inode *inode);
+extern blk64_t ext2fs_count_blocks(ext2_filsys fs, ext2_ino_t ino,
+					struct ext2_inode *inode);
+extern errcode_t ext2fs_decode_extent(struct ext2fs_extent *to, void *from,
+				      int len);
 
 /* fallocate.c */
 #define EXT2_FALLOCATE_ZERO_BLOCKS	(0x1)
